e1a58d8072d51f949c3aecaabad4e50d80b515d1
Fred <alcide.fred@gmail.com>
2018-01-27 17:00:31 +0100

    Proto Fin

--- Assets/Scripts/DeletePrefab.cs
+++ Assets/Scripts/DeletePrefab.cs
@@ -2,14 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeletePrefab : MonoBehaviour {
-
-	public float lifespan = 2f;
-
-	void Update () {
-		lifespan -= Time.deltaTime;
-		if(lifespan <= 0f) {
-			Destroy(gameObject);
-		}		
+public class DeletePrefab : MonoBehaviour
+{
+	public void DestroyObject() {
+		Destroy(gameObject);
 	}
 }
